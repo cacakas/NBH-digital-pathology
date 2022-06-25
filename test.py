84cc6cb54f15818fca0602c39a745eeb37dee329
Byeong-Joo Noh <cakas@hanmail.net>
2022-06-25 19:13:47 +0900

    commit second

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 # python test 
 # python test 
 # python test 
+# 이부분 수정했다.
 # python test 
 # python test 
 # python test 
 # python test 
 # python test 
-# python test 
-# python test 
+# 여기도 수정했다.
+
+# 이곳은 추가했다.
